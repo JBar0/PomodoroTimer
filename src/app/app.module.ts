5f561912e10e5f39200b04dcbe7a35b378bf94ab
JBar0 <baranowski.jakub97@gmail.com>
2020-06-07 12:24:42 +0200

    Everything fucked up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule
